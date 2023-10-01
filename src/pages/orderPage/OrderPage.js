9718357a8a514172a173681cac90201a8c716175
runtimeerror55 <runtimeerror55@gmail.com>
2023-10-01 23:39:00 +0530

    some changes

--- src/pages/orderPage/OrderPage.js
+++
@@ -1,7 +0,0 @@
-export const orderDataLoader = async ({ params }) => {
-      const response = await fetch(
-            `http://localhost:3000/account/orders/${params.orderId}`
-      );
-      const data = await response.json();
-      return data;
-};
